e4ac168e24d545fce49689301989969ef0618617
qslia <qsliang456@163.com>
2022-06-12 11:28:47 +0800

    rolling dice of different sizes

--- dice_visual.py
+++ dice_visual.py
@@ -3,10 +3,10 @@ from plotly import offline
 from die import Die
 
 die_1 = Die()
-die_2 = Die()
+die_2 = Die(10)
 
 results = []
-for roll_num in range(1000):
+for roll_num in range(50000):
     result = die_1.roll() + die_2.roll()
     results.append(result)
 
@@ -21,6 +21,6 @@ data = [Bar(x=x_values, y=frequencies)]
 
 x_axis_config = {'title': 'Result', 'dtick': 1}
 y_axis_config = {'title': 'Frequency of Result'}
-my_layout = Layout(title='Results of rolling two D6 dice 1000 times',
-                   xaxis=y_axis_config, yaxis=y_axis_config)
-offline.plot({'data': data, 'layout': my_layout}, filename='d6_d6.html')
+my_layout = Layout(title='Results of rolling a D6 and a D10 50000 times',
+                   xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({'data': data, 'layout': my_layout}, filename='d6_d10.html')
